75c4cbf1c66e9068284542e11c1e475fdfc2b713
Judy-Zhu <zhudan_judy@aliyun.com>
2017-11-04 15:34:20 -0700

    Update travis file

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -1,4 +1,10 @@
 <?php
+    if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+	}
+	
+
  class TaskTest extends PHPUnit_Framework_TestCase
   {  
   
